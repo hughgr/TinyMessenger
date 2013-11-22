d769c9275d7b59b382990bf56b9958fe33005416
xin.shux <xin.shux@taobao.com>
2013-11-22 16:29:41 +0800

    bug fix

--- js/TinyMessenger.js
+++ js/TinyMessenger.js
@@ -1,30 +1,29 @@
 var TinyMessenger = function(){
 }
-TinyMessenger.prototype.sendMsg = function(){
-    var args = Array.prototype.slice.call(arguments,0);
-    var ev = args.shift();
-    var list, calls;
-    if(!(calls = this._callbacks)) return this;
-    if(!(list = this._callbacks[ev])) return this;
-    for(var i = 0; i < list.length; i++){
-        list[i].apply(this, args);
-    }
-    return this;
-};
-TinyMessenger.prototype.getMsg = function(ev, callback){
+TinyMessenger.prototype.getMsg = function(customEvent, callback){
     var pattern = /(.[^:]+):(\w+)/gi;
-    ev.replace(pattern,function(all,dom,domEv){
+    customEvent.replace(pattern,function(all,dom,domEv){
             $(dom).on(domEv,function(){
                 callback.apply(this,arguments)
             })
     })
     this._callbacks || (this._callbacks = {});
-    (this._callbacks[ev] || (this._callbacks[ev] = [])).push(callback);
+    (this._callbacks[customEvent] || (this._callbacks[customEvent] = [])).push(callback);
     return this;
 };
-TinyMessenger.prototype.cancelGetMsg = function(ev,callback){
-    if(!(this._callbacks) || !(this._callbacks[ev])) return this;
-    this._callbacks[ev].splice(this._callbacks[ev].indexOf(callback),1);
+TinyMessenger.prototype.sendMsg = function(){
+    var args = Array.prototype.slice.call(arguments,0),
+        customEvent = args.shift(),
+        list;
+    if(!this._callbacks) return this;
+    if(!(list = this._callbacks[customEvent])) return this;
+    for(var i = 0; i < list.length; i++){
+        list[i].apply(this, args);
+    }
     return this;
-
+};
+TinyMessenger.prototype.cancelGetMsg = function(customEvent,callback){
+    if(!(this._callbacks) || !(this._callbacks[customEvent])) return this;
+    this._callbacks[customEvent].splice(this._callbacks[customEvent].indexOf(callback),1);
+    return this;
 }
